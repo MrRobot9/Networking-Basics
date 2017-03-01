8eaa3c9968b985d1570bca639750478c5b326a7b
MrRobot9 <arjunhegde777@gmail.com>
2017-03-01 08:57:33 +0530

    Initial commit

---
+++ basic-client-server/tcpServer.py
@@ -0,0 +1,28 @@
+import socket
+
+def main():
+	host = "127.0.0.1"
+	port = 8080
+
+	s = socket.socket()
+	s.bind((host,port))
+
+	s.listen(1)
+	c , addr = s.accept()
+	print "Connection from:",str(addr)
+
+	while True:
+		data = c.recv(1024)
+		if not data:
+			break
+		print "from user" + str(data)
+		data = str(data).upper()
+		print "Sending:"+ str(data)
+		c.send(data)
+	c.close()
+
+
+if __name__ == '__main__':
+	main()
+
+
